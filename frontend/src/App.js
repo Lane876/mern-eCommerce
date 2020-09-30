0122fcffbcb918c21402628b55e573db87c9f6e9
Milan Milenkovic <milenkovicmilan10@gmail.com>
2020-09-30 20:41:22 +0200

    :fire:

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,23 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 
 function App() {
   return (
-    <>
-      <h1>Welcome to eCommerce</h1>
-    </>
+    <Router>
+      <Header />
+      <Container>
+        <main className="py-3">
+          <Route path="/" exact component={HomeScreen} />
+          <Route path="/product/:id" component={ProductScreen} />
+        </main>
+      </Container>
+      <Footer />
+    </Router>
   );
 }
 
