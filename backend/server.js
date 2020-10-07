9a5753e8530e99f3a73005c984be5ea4e85d65d1
Milan Milenkovic <milenkovicmilan10@gmail.com>
2020-10-07 20:02:22 +0200

    :fire:

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
 import colors from "colors";
-import products from "./data/products.js";
 import connectDB from "./config/db.js";
+import productsRoutes from "./routes/productsRoutes.js";
+import { errorHandler, notFound } from "./middleware/errorMiddlewre.js";
 
 dotenv.config();
 
@@ -13,14 +14,11 @@ app.get("/", (req, res) => {
   res.send("API IS RUNNING...");
 });
 
-app.get("/api/products", (req, res) => {
-  res.json(products);
-});
+app.use("/api/products", productsRoutes);
 
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
+app.use(notFound);
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
