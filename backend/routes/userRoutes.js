3d638ecd6cff54ec25f3c657be6c3323b8a13851
Milan Milenkovic <milenkovicmilan10@gmail.com>
2020-10-19 20:19:13 +0200

    :fire:

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -4,15 +4,25 @@ import {
   getUserProfile,
   registerUser,
   updateUserProfile,
+  getUsers,
+  deletetUser,
+  getUserById,
+  updateUser,
 } from "../controllers/userController.js";
-import { protect } from "../middleware/authMiddleware.js";
+import { protect, admin } from "../middleware/authMiddleware.js";
 const router = express.Router();
 
-router.route("/").post(registerUser);
+router.route("/").post(registerUser).get(protect, admin, getUsers);
 router.post("/login", authUser);
 router
   .route("/profile")
   .get(protect, getUserProfile)
   .put(protect, updateUserProfile);
+
+router
+  .route("/:id")
+  .delete(protect, admin, deletetUser)
+  .get(protect, admin, getUserById)
+  .put(protect, admin, updateUser);
 
 export default router;
